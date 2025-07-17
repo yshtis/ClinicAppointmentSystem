42a1a98057e36147e9c9ffd60eb549e53ced6aae
yshtis <yshtis404@gmail.com>
2025-07-18 06:19:04 +0900

    ログイン画面の動的要素追加

--- src/main/java/com/unknownclinic/appointment/ClinicAppointmentSystemApplication.java
+++ src/main/java/com/unknownclinic/appointment/ClinicAppointmentSystemApplication.java
@@ -1,9 +1,11 @@
 package com.unknownclinic.appointment;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.unknownclinic.appointment.repository")
 public class ClinicAppointmentSystemApplication {
 
 	public static void main(String[] args) {
